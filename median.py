f7390c4900595285f20fcbe4b9f666b4d9806624
Cecilia Zhou <zilingzhou@college.harvard.edu>
2015-04-17 03:17:11 -0400

    modified median

--- median.py
+++ median.py
@@ -1,26 +1,26 @@
 import random
 
-A = list(map(int, raw_input().split()))
+lst = list(map(int, raw_input().split()))
 #median, or if even number of items in left, 
 #the item to the left of the median
-k = len(A)/2 
 def quick_select(A, k):
+    lesser = []
+    greater = []
     pivot = random.choice(A)
-
-    A1 = []
-    A2 = []
-
     for i in A:
         if i < pivot:
-            A1.append(i)
+            lesser.append(i)
         else:
-            A2.append(i)
-
-    if k < len(A1):
-        return quick_select(A1, k)
-    elif k > len(A) - len(A2):
-        return quick_select(A2, k - (len(A) - len(A2)))
+            greater.append(i)
+    (a, a1, a2) = (len(A), len(lesser), len(greater))
+    if k < a1:
+        return quick_select(lesser, k)
+    elif k > a - a2:
+        return quick_select(greater, k - (a - a2))
     else:
         return pivot
 
-print quick_select (A, k)
+def median_1 (C):
+   print quick_select (C, len(C)/2)
+
+median_1 (lst)
