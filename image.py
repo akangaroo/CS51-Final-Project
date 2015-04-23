bd7d81ccdeefc5fc382703073d4ebf6abd016ed2
akangaroo <akamy7@gmail.com>
2015-04-22 22:50:29 -0400

    image class compiles

--- image.py
+++ image.py
@@ -2,19 +2,19 @@ from PIL import Image
 
 class image(object):
 
-	#initialize image object
-	def __init__(self, filename):
-		self.img = Image.open(filename)
-		self.rgb_img = img.convert('RGB')
-		self.width, self.height = self.img.size
+    #initialize image object
+    def __init__ (self, filename):
+        self.img = Image.open(filename)
+        self.rgb_img = img.convert('RGB')
+        self.width, self.height = self.img.size
 
-	#pos should always be a tuple of two integers
-	def get_RGB_value(self, pos):
-		return self.rgb_img.getpixel(pos)
+    #pos should always be a tuple of two integers
+    def get_RGB_value(self, pos):
+        return self.rgb_img.getpixel(pos)
 
-	def get_next_RGB(self, pos):
-		(x, y) = pos
-		if x >= self.width:
-			return self.get_RGB_value(self, (1, y+1))
-		else:
-			return self.get_RGB_value(self, (x+1, y))
+    def get_next_RGB(self, pos):
+        (x, y) = pos
+        if x >= self.width:
+            return self.get_RGB_value(self, (1, y+1))
+        else:
+            return self.get_RGB_value(self, (x+1, y))
