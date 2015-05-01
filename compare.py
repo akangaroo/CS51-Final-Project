87bb906b19b8fc8177b99c6e84b063d034a5509e
akangaroo <akamy7@gmail.com>
2015-05-01 03:49:32 -0400

    adding renamed files

--- compare.py
+++ compare.py
@@ -14,9 +14,9 @@ def brute(image, base):
 			rgb = [abs(rb-r),abs(gb-g),abs(bb-b)]
 			if sum(rgb) <= 10:
 				count = count+1
-	print count
-	print size_x*size_y
-	print  (float (count) / (size_x * size_y))
+	# print count
+	# print size_x*size_y
+	# print  (float (count) / (size_x * size_y))
 	return '%.2f' % (float(count) / (size_x * size_y) * 100)
 
 model = Picture(filename = os.path.join(IMAGE_DIRECTORY, 'final.png'))
